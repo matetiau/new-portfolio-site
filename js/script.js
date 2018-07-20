8e80876bb41efddc7aaf18a1a00a7e5c96867cc6
matetiau <matekamer@gmail.com>
2018-07-20 17:09:46 +0300

    added animation and changed wallper

--- js/script.js
+++ js/script.js
@@ -4,25 +4,24 @@ window.onscroll = function() {
        else {
         document.getElementById("navbar").className = "roll-up";
       }
-   
 };
 
-{
 window.sr = ScrollReveal();
+window.onscroll = function() {
+    if (document.documentElement.scrollTop > 1000 ){
+        document.getElementById('link-left').style.animation =' mymove 1.2s';
+        document.getElementById('link-left').style.visibility='visible';
+    }
+}
 
-sr.reveal('.mee', { duration: 3000 });
-sr.reveal('.aboutme', { duration: 5000 });
-sr.reveal('.photoOff', { duration: 7000 });
 sr.reveal('.item1', {duration: 1200 }, 320);
-sr.reveal('.project-tile', {duration: 1500 }, 350);
-}
 
 
-function koko() {
-    document.getElementById('linkaa').style.backgroundColor = "#F7C502";
-    document.getElementById('linkaa').style.color = "aliceblue";
-}
-    
+
+
+
+
+
 
 
 
