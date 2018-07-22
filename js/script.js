8f72a2ad333cc2b7b27f31c3bbd7aa0ff6fae804
matetiau <matekamer@gmail.com>
2018-07-22 19:40:19 +0300

    added animation for buttons

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 window.onscroll = function() {
     /*navbar animation*/
     if ( document.documentElement.scrollTop > 400) {
-         document.getElementById("navbar").className = "roll-down";}
+         document.getElementById("navbar").className = "roll-down";
+        
+    
+    }
        else {
         document.getElementById("navbar").className = "roll-up";
       }
@@ -22,7 +25,29 @@ window.onscroll = function() {
         document.getElementById('link-center').style.visibility='visible';
         
         
+        
     }
+    
+    if (document.documentElement.scrollTop > 500 && document.documentElement.scrollTop < 800  ){
+    document.getElementById('kompo').style.backgroundColor ='#F7C502';}
+    document.getElementById('portfoliooo').style.backgroundColor ='inherit';
+    document.getElementById('kola').style.backgroundColor ='inherit';
+    
+    if (document.documentElement.scrollTop >900 && document.documentElement.scrollTop < 1200 ){
+    document.getElementById('portfoliooo').style.backgroundColor ='#F7C502';
+    document.getElementById('kompo').style.backgroundColor ='inherit';
+    }
+    
+    if (document.documentElement.scrollTop > 1300 ){
+    document.getElementById('portfoliooo').style.backgroundColor ='inherit';
+    document.getElementById('kola').style.backgroundColor ='#F7C502';
+    
+    }
+    if (document.documentElement.scrollTop < 300){
+        document.getElementById('kompo').style.backgroundColor ='inherit';
+    }
+    
+    
 }
 
 /*skills animation*/
