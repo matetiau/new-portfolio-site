c40218cb35ab5bf0bb2bed9abf91399046e4e24e
matetiau <matekamer@gmail.com>
2018-07-22 15:58:38 +0300

    added some styles and animations

--- js/script.js
+++ js/script.js
@@ -1,29 +1,32 @@
 window.onscroll = function() {
-  if ( document.documentElement.scrollTop > 400) {
+    /*navbar animation*/
+    if ( document.documentElement.scrollTop > 400) {
          document.getElementById("navbar").className = "roll-down";}
        else {
         document.getElementById("navbar").className = "roll-up";
       }
-};
-
-window.sr = ScrollReveal();
-window.onscroll = function() {
-    if (document.documentElement.scrollTop > 1100 ){
-        document.getElementById('link-left').style.animation =' mymove 1.2s';
+    
+    
+    
+    /*portfolio animation*/
+    if (document.documentElement.scrollTop > 900 ){
+        document.getElementById('link-left').style.animation =' mymove 1s';
         document.getElementById('link-left').style.visibility='visible';
         
         
-        document.getElementById('link-right').style.animation =' mymoveleft 1.2s';
+        document.getElementById('link-right').style.animation =' mymoveleft 1s';
         document.getElementById('link-right').style.visibility='visible';
         
         
-        document.getElementById('link-center').style.animation =' mymovecenter 1.2s';
+        document.getElementById('link-center').style.animation =' mymovecenter 1s';
         document.getElementById('link-center').style.visibility='visible';
         
         
     }
 }
 
+/*skills animation*/
+window.sr = ScrollReveal();
 sr.reveal('.item1', {duration: 1200 }, 320);
 
 
