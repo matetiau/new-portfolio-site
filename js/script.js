dead6b973668a4acadfd0388204557a3110d6595
matetiau <matekamer@gmail.com>
2018-07-22 23:56:05 +0300

    some changes to sizes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+$(document).ready(function()
+                  {
 window.onscroll = function() {
     /*navbar animation*/
     if ( document.documentElement.scrollTop > 400) {
@@ -33,17 +35,17 @@ window.onscroll = function() {
     document.getElementById('portfoliooo').style.backgroundColor ='inherit';
     document.getElementById('kola').style.backgroundColor ='inherit';
     
-    if (document.documentElement.scrollTop >900 && document.documentElement.scrollTop < 1200 ){
+    if (document.documentElement.scrollTop >1000 && document.documentElement.scrollTop < 1400 ){
     document.getElementById('portfoliooo').style.backgroundColor ='#F7C502';
     document.getElementById('kompo').style.backgroundColor ='inherit';
     }
     
-    if (document.documentElement.scrollTop > 1300 ){
+    if (document.documentElement.scrollTop > 1401 ){
     document.getElementById('portfoliooo').style.backgroundColor ='inherit';
     document.getElementById('kola').style.backgroundColor ='#F7C502';
     
     }
-    if (document.documentElement.scrollTop < 300){
+    if (document.documentElement.scrollTop < 300 || document.documentElement.scrollTop > 1301 ){
         document.getElementById('kompo').style.backgroundColor ='inherit';
     }
     
@@ -53,6 +55,8 @@ window.onscroll = function() {
 /*skills animation*/
 window.sr = ScrollReveal();
 sr.reveal('.item1', {duration: 1200 }, 320);
+    
+});
 
 
 
